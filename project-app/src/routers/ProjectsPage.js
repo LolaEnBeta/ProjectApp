4f87a90ee9e8aeb69d9712680910ff76acb5a5a6
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-17 17:35:59 +0200

    Map the projects to create their buttons

--- project-app/src/routers/ProjectsPage.js
+++ project-app/src/routers/ProjectsPage.js
@@ -3,10 +3,21 @@ import React from 'react';
 class ProjectsPage extends React.Component {
     constructor(){
         super();
+        this.state = {
+            projects: [
+                "project1",
+                "proyect2",
+            ]
+        }
     }
     render() {
         return (
-            <div> Hello world </div>
+            <div>
+                {this.state.projects.map(project => {
+                    return (
+                        <button>{project}</button>);
+                })}
+            </div>
         );
     }
 }
