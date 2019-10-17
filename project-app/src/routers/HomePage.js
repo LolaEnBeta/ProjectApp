f1c0000c9c4c1026fb114899163076d335375fda
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-17 18:11:08 +0200

    Create HomePage router

---
+++ project-app/src/routers/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <button>PROJECTS</button>
+        );
+    }
+}
+
+export default HomePage;
