17f82b123eb59e28a73f132ed042057d042a48cc
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-17 17:26:08 +0200

    Create a ProjectsPage class in routers foulder

---
+++ project-app/src/routers/ProjectsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ProjectsPage extends React.Component {
+    constructor(){
+        super();
+    }
+    render() {
+        return (
+            <div> Hello world </div>
+        );
+    }
+}
+
+export default ProjectsPage;
