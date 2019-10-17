355ebaa8ecbab42ca770343645b3627f340bbcb8
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-17 18:12:45 +0200

    Add HomePage to principal route page

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import HomePage from './routers/HomePage';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to="/">Home</Link>
+          </nav>
+          <div>
+            <Route exact path="/" component={HomePage}/>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
