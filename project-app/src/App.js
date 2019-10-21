7ef7f9770d550ba7f29d0e95cf31883017e47f49
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-21 13:40:53 +0200

    Ceate a Route for ProjectsPage

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import HomePage from './routers/HomePage';
-
+import ProjectsPage from './routers/ProjectsPage';
 
 class App extends React.Component {
   constructor() {
@@ -15,9 +15,11 @@ class App extends React.Component {
         <div className="App">
           <nav>
             <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
           </nav>
           <div>
             <Route exact path="/" component={HomePage}/>
+            <Route path="/projects" component={ProjectsPage}/>
           </div>
         </div>
       </Router>
