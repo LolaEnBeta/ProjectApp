d77941ed6405243081b525d34f486331ff6fd38d
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-21 15:21:11 +0200

    Create a ProjectPage router

---
+++ project-app/src/routers/ProjectPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ProjectPage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>
+                PROJECT
+            </div>
+        );
+    }
+}
+
+export default ProjectPage;
