fa9f800ba3fbddd620cee8e8f0d94905e47beceb
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-21 16:13:19 +0200

    Create a FormPage router

---
+++ project-app/src/routers/FormPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProjectForm from '../components/ProjectForm';
+
+class FormPage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <ProjectForm></ProjectForm>
+        );
+    }
+}
+
+export default FormPage;
