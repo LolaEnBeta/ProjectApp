df9c314617c10b908a0e7ab7c057a241a7881ce5
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-21 15:33:48 +0200

    Add style with Bootstrap to ProjectPage

--- project-app/src/routers/ProjectPage.js
+++ project-app/src/routers/ProjectPage.js
@@ -7,8 +7,19 @@ class ProjectPage extends React.Component {
 
     render() {
         return(
-            <div>
-                PROJECT
+            <div class="card text-center">
+                <div class="card-header">
+                    PROJECT
+                </div>
+
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
             </div>
         );
     }
