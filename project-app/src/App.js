c967dbe1ba9d009170e0c3a802db279b00a04e1b
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-21 16:32:15 +0200

    Change ProjectsPage to Home path Route

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -17,12 +17,10 @@ class App extends React.Component {
         <div className="App">
           <nav className="navbar navbar-expand-lg">
             <Link className="btn btn-light m-2" to="/">Home</Link>
-            <Link className="btn btn-light m-2" to="/projects">Projects</Link>
             <Link className="btn btn-light m-2" to="/projectform">Add new project</Link>
           </nav>
           <div>
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/projects" component={ProjectsPage}/>
+            <Route exact path="/" component={ProjectsPage}/>
             <Route path="/project" component={ProjectPage}/>
             <Route path="/projectform" component={FormPage}/>
           </div>
