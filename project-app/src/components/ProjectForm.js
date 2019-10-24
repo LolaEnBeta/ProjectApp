08e25a154b62d5388e956d20eeb19c89f86f8b34
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-24 19:26:23 +0200

    Add "description" input in ProjectForm

--- project-app/src/components/ProjectForm.js
+++ project-app/src/components/ProjectForm.js
@@ -20,16 +20,35 @@ class ProjectForm extends React.Component {
                 <form
                     // onSubmit={this.addProject.bind(this)}
                 >
-                    <input
-                        onChange={event => this.changeStateValues(event.target)}
-                        type="text"
-                        name="name"
-                        placeholder="Project name"
-                    />
-                    <input
-                        className="btn"
-                        type="submit"
-                        value="Create"/>
+                    <div className="card m-2" style={{width: 250}}>
+                        <div>
+                            <input
+                                onChange={event => this.changeStateValues(event.target)}
+                                type="text"
+                                name="name"
+                                placeholder="Project name"
+                                className="mt-4"
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                onChange={event => this.changeStateValues(event.target)}
+                                type="text"
+                                placeholder="Description"
+                                className="m-2"
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                className="btn"
+                                type="submit"
+                                value="Create"
+                                className="btn btn-light m-2"
+                            />
+                        </div>
+                    </div>
                 </form>
             </div>
         );
