cc0dc258b6da05d37b2e8210715eeb36077f19fa
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-24 23:24:37 +0200

    Pass dateOfCreation with props to ProjectMiniature

--- project-app/src/routers/AllProjectsPage.js
+++ project-app/src/routers/AllProjectsPage.js
@@ -9,10 +9,12 @@ class AllProjectsPage extends React.Component {
                 {
                     name: "project1",
                     description: "new project",
+                    dateOfCreation: "24-10-2019",
                 },
                 {
                     name: "project2",
                     description: "another project",
+                    dateOfCreation: "31-10-2015",
                 },
             ]
         }
@@ -23,8 +25,10 @@ class AllProjectsPage extends React.Component {
                 {this.state.projects.map(project => {
                     return (
                         <ProjectMiniature
+                            key={project.name}
                             name={project.name}
-                            description={project.description}/>);
+                            description={project.description}
+                            dateOfCreation={project.dateOfCreation}/>);
                 })}
             </div>
         );
