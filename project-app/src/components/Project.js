646b4da7d9f3b0cfe54690f03f23424d173c6149
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-24 23:02:00 +0200

    Pass the project description with location to Project component

--- project-app/src/components/Project.js
+++ project-app/src/components/Project.js
@@ -12,7 +12,7 @@ class Project extends React.Component {
 
     render() {
         return(
-            <div className="card text-center mx-auto mb-4" style={{width: 250}}>
+            <div className="card text-center mx-auto mb-4 mt-4" style={{width: 250}}>
                 <div>
                     <div class="card-header">
                         {this.state.name}
@@ -30,7 +30,7 @@ class Project extends React.Component {
                 <div>
                     <Link
                         className="btn btn-light m-2"
-                        to={"/project?name=" + this.state.name}>
+                        to={"/project?name=" + this.state.name + "&description=" + this.state.description}>
                         Open project
                     </Link>
                 </div>
