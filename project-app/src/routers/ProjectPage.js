d73487636a402466b5de63f39145c930f1af6855
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-24 23:32:28 +0200

    Pass dateOfCreation with props to Project component

--- project-app/src/routers/ProjectPage.js
+++ project-app/src/routers/ProjectPage.js
@@ -7,9 +7,11 @@ class ProjectPage extends React.Component {
         const params = new URLSearchParams(location.search);
         let name = params.get("name");
         let description = params.get("description");
+        let dateOfCreation = params.get("dateOfCreation");
         this.state = {
             name: name,
             description: description,
+            dateOfCreation: dateOfCreation
         }
     }
 
@@ -19,6 +21,8 @@ class ProjectPage extends React.Component {
                 <Project
                     name={this.state.name}
                     description={this.state.description}
+                    dateOfCreation={this.state.dateOfCreation}
+
                 />
             </div>
         );
